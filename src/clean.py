5f7b7c507a42d47dbd0642e7e154511e60472e82
Uladzislau Mikhayevich <uladzislau.mikhayevich@stud.esdc.lt>
2026-05-28 20:26:51 +0300

    fix: steps
    clean

--- src/clean.py
+++ src/clean.py
@@ -5,28 +5,30 @@ import numpy as np
 import cv2
 
 
-def remove_small_components(mask, min_area=80):
-    print(f"{MODULE_NAME}: Remove small components (min area={min_area})")
-    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(mask, 8)
-    cleaned = np.zeros_like(mask)
-
-    for i in range(1, num_labels):
-        area = stats[i, cv2.CC_STAT_AREA]
-        if area >= min_area:
-            cleaned[labels == i] = 255
-
-    return cleaned
-
 def clean_mask(mask):
     print(f"{MODULE_NAME}: Cleaning mask...")
-    mask = (mask > 0).astype(np.uint8) * 255
 
-    open_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    close_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    if len(mask.shape) == 2:
+        gray = mask
+    else:
+        gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 
-    cleaned = cv2.morphologyEx(mask, cv2.MORPH_OPEN, open_kernel, iterations=1)
-    cleaned = cv2.morphologyEx(cleaned, cv2.MORPH_CLOSE, close_kernel, iterations=2)
-    cleaned = remove_small_components(cleaned, min_area=100)
+    blur = cv2.GaussianBlur(gray, (15, 15), 0)
+
+    blur = cv2.normalize(
+        blur, None, 0, 255, cv2.NORM_MINMAX
+    ).astype(np.uint8)
+
+    edges = cv2.Canny(blur, threshold1=50, threshold2=150)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+    edges = cv2.morphologyEx(
+        edges, cv2.MORPH_CLOSE, kernel, iterations=1
+    )
+
+    edges = cv2.dilate(edges, kernel, iterations=1)
 
     print(f"{MODULE_NAME}: Mask cleaned")
-    return cleaned
+
+    return edges
